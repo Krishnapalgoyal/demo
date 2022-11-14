5bf7c59e9fba3632985f846646419140e6849fd1
Krishnapalgoyal <Krishnapal.tout@gmail.com>
2022-11-14 13:08:22 +0530

    solve the bug of Jquery validation in login form passwoed field

--- app/javascript/packs/example.js
+++ app/javascript/packs/example.js
@@ -1,1 +1,12 @@
-
+import "jquery-validation"
+$(document).ready(function() {
+ $("#new_employee").validate({
+    rules:{
+            'password':{
+                max: 10,
+                min: 6,
+                required: true
+              }
+            }
+ });
+});
